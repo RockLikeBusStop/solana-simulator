a53dd60eea42da489c588496cf8d123aac53cea6
OpenPendulum <arasam1067@gmail.com>
2024-06-20 21:31:00 -0400

    add black & flake8 workflow steps

--- src/solsim/simulation.py
+++ src/solsim/simulation.py
@@ -1,6 +1,6 @@
 import asyncio
 from collections.abc import Iterable
-from typing import Dict, List, Set, Union
+from typing import Union
 
 import pandas as pd
 from tqdm.auto import tqdm
@@ -14,7 +14,6 @@ class Simulation:
         self._system = system
         self._watchlist = set(watchlist)
         self._n_steps = n_steps
-        self._system_uses_solana = isinstance(self._system, BaseSolanaSystem)
 
     def run(self) -> pd.DataFrame:
         return asyncio.run(self._run())
@@ -24,29 +23,27 @@ class Simulation:
             state: StateType = {}
             history: list[StateType] = []
             results: list[StateType] = []
-            for step in tqdm(range(-1, self._n_steps), desc="Steps completed"):
-                if step == -1:
-                    updates = (
-                        await self._system.initialStep()
-                        if self._system_uses_solana
-                        else self._system.initialStep()
-                    )
+            for step in tqdm(range(-1, self._n_steps - 1), desc="Steps completed"):
+                if self._system.uses_solana:
+                    updates = await self._system.initialStep() if step == -1 else await self._system.step(state, history)
                 else:
-                    updates = (
-                        await self._system.step(state, history)
-                        if self._system_uses_solana
-                        else self._system.step(state, history)
-                    )
+                    updates = self._system.initialStep() if step == -1 else self._system.step(state, history)
                 state = {**state, **updates, "step": step}
                 history.append(state)
                 results.append(self._filter_state(state))
         finally:
-            if self._system_uses_solana:
-                await self._system.tearDown()
-        return pd.DataFrame(results)
+            if self._system.uses_solana:
+                await self._system.tearDown()  # type: ignore
+        results = pd.DataFrame(results)
+        return self._reorder_results_columns(results)
 
     def _filter_state(self, state: StateType) -> StateType:
         for qty in self._watchlist:
             if qty not in state:
                 raise Exception(f"{qty} not found in state: {state}")
         return {qty: state[qty] for qty in self._watchlist | {"step"}}
+
+    @staticmethod
+    def _reorder_results_columns(results: pd.DataFrame) -> pd.DataFrame:
+        cols = ["step"] + sorted([col for col in results.columns if col != "step"])
+        return results[cols]
--- src/solsim/system.py
+++ src/solsim/system.py
@@ -5,9 +5,10 @@ import signal
 import subprocess
 from subprocess import DEVNULL
 import tempfile
-from typing import Awaitable, Dict, Optional, Set, List, Any, Union
+from typing import Awaitable, Optional, List, Any
 
 from anchorpy import create_workspace, close_workspace
+from psutil import Process
 from solana.publickey import PublicKey
 from solana.rpc.api import Client
 from solana.rpc import commitment
@@ -24,27 +25,40 @@ class BaseSystem(ABC):
     def step(self, state: StateType, history: List[StateType]) -> Awaitable[Any]:
         raise NotImplementedError
 
+    @property
+    def uses_solana(self) -> bool:
+        return isinstance(self, BaseSolanaSystem)
+
 
 class BaseSolanaSystem(BaseSystem):
 
     SOLANA_CLUSTER_URI = "http://127.0.0.1:8899"
 
-    def __init__(self, workspace_dir: str, client: Optional[Client] = None, start_localnet: bool = True) -> None:
-        if start_localnet:
+    def __init__(
+        self, workspace_dir: str, client: Optional[Client] = None, localnet_process: Optional[Process] = None
+    ) -> None:
+        if not localnet_process:
             self._logfile = tempfile.NamedTemporaryFile()
-            self._localnet = subprocess.Popen(['anchor', 'localnet'], cwd=workspace_dir, stdout=self._logfile, stderr=DEVNULL)
-            print('Waiting for Solana localnet cluster to start (~10s) ...')
+            self._localnet = self._start_localnet(workspace_dir)
+            print("Waiting for Solana localnet cluster to start (~10s) ...")
             while not self._localnet_ready:
                 time.sleep(1)
         else:
-            self._localnet = None
+            self._localnet = localnet_process
+
         self.workspace = create_workspace(workspace_dir)
         self.client = client or Client(self.SOLANA_CLUSTER_URI)
 
+    def _start_localnet(self, workspace_dir: str) -> subprocess.Popen[Any]:
+        for proc in psutil.process_iter():
+            if proc.name() == "solana-test-validator":
+                self._terminate_processes([proc])
+        return subprocess.Popen(["anchor", "localnet"], cwd=workspace_dir, stdout=self._logfile, stderr=DEVNULL)
+
     @property
-    def _localnet_ready(self):
+    def _localnet_ready(self) -> bool:
         lastline = subprocess.check_output(["tail", "-n", "1", self._logfile.name]).decode("utf-8").strip()
-        return '| Processed Slot: ' in lastline
+        return "| Processed Slot: " in lastline
 
     async def tearDown(self) -> None:
         await close_workspace(self.workspace)
@@ -56,35 +70,37 @@ class BaseSolanaSystem(BaseSystem):
     ) -> float:
         return float(self.client.get_token_account_balance(pubkey, commitment)["result"]["value"]["uiAmount"])
 
-    def _terminate_localnet(self, timeout=10) -> None:
+    def _terminate_processes(self, kill_list: list[Process], timeout: int = 10) -> None:
+        # Attempt graceful termination first.
+        for p in reversed(kill_list):
+            self._signal_process(p, signal.SIGTERM)
+        _, alive = psutil.wait_procs(kill_list, timeout=timeout)
+
+        # Forcefully terminate procs still running.
+        for p in alive:
+            self._signal_process(p, signal.SIGKILL)
+        _, alive = psutil.wait_procs(kill_list, timeout=timeout)
+
+        if alive:
+            raise Exception(f"could not terminated process {alive}")
+
+    def _terminate_localnet(self) -> None:
         """
-        Borrowed from https://github.com/pytest-dev/pytest-xprocess/blob/6dac644e7b6b17d9b970f6e9e2bf2ade539841dc/xprocess/xprocess.py#L35.
+        Borrowed from https://github.com/pytest-dev/pytest-xprocess/blob/6dac644e7b6b17d9b970f6e9e2bf2ade539841dc/xprocess/xprocess.py#L35.  # noqa E501
         """
         parent = psutil.Process(self._localnet.pid)
         try:
             kill_list = [parent]
             kill_list += parent.children(recursive=True)
-
-            # Attempt graceful termination first.
-            for p in reversed(kill_list):
-                self._signal_process(p, signal.SIGTERM)
-            _, alive = psutil.wait_procs(kill_list, timeout=timeout)
-
-            # Forcefully terminate procs still running.
-            for p in alive:
-                self._signal_process(p, signal.SIGKILL)
-            _, alive = psutil.wait_procs(kill_list, timeout=timeout)
-
-            if alive:
-                raise Exception(f"could not terminated process {alive}")
+            self._terminate_processes(kill_list)
         except (psutil.Error, ValueError) as err:
             raise Exception(f"Error while terminating process {err}")
         else:
             return
 
-    def _signal_process(self, p, sig):
+    def _signal_process(self, p: Process, sig: signal.Signals) -> None:
         """
-        Borrowed from: https://github.com/pytest-dev/pytest-xprocess/blob/6dac644e7b6b17d9b970f6e9e2bf2ade539841dc/xprocess/xprocess.py#L29.
+        Borrowed from: https://github.com/pytest-dev/pytest-xprocess/blob/6dac644e7b6b17d9b970f6e9e2bf2ade539841dc/xprocess/xprocess.py#L29.  # noqa E501
         """
         try:
             p.send_signal(sig)
